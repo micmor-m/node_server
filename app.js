08323f59cb73ba1d99ba96dcd7aae099003ea5a7
mic <micmor.m@gmail.com>
2020-10-29 21:49:00 -0400

    npm init and nodemon

--- app.js
+++ app.js
@@ -3,4 +3,6 @@ const routes = require("./routes");
 
 const server = http.createServer(routes);
 
-server.listen(3000);
+server.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
